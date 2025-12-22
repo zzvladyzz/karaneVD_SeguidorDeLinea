47b8e9e8f03ab12d6880537294b687e23ad5f09e
zzvladyzz <vladimir_1913@hotmail.com>
2025-12-22 03:16:10 +0100

    Aplicar .gitignore a archivos inecesarios

--- Debug/Core/Src/LIB_FiltroMadgWick.d
+++
@@ -1,4 +0,0 @@
-Core/Src/LIB_FiltroMadgWick.o: ../Core/Src/LIB_FiltroMadgWick.c \
- ../Core/Inc/LIB_FiltroMadgWick.h ../Core/Inc/LIB_MPU6500_SPI.h
-../Core/Inc/LIB_FiltroMadgWick.h:
-../Core/Inc/LIB_MPU6500_SPI.h:
